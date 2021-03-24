46a77c4487db08d5a7e6dc5b987f2a5999f71cc9
Himanshu Goel <himanshugoelnasa2@gmail.com>
2021-03-24 18:35:18 +0530

    commit

--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL'),    
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),     
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'lara8'),
             'username' => env('DB_USERNAME', 'root'),     
