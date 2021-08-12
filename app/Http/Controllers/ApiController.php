2b27c684a564bd5a970987ecd3226d8ccb292ec5
Himanshu Goel <Himanshugoelnasa2@gmail.com>
2021-08-12 11:49:23 +0530

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,1 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function test_api()
+    {
+        $ip = $_SERVER['REMOTE_HOST'];
+        return response()->json(['ip' : $ip]);
+    }
+}
 
