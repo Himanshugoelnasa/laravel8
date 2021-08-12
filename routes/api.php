532db55c587460a475b757b6524dbfb2db1aa38c
Himanshu Goel <himanshugoelnasa2@gmail.com>
2021-08-12 11:57:19 +0530

    ok

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/test-api', [ApiController::class, 'test_api'])
+Route::get('/test-api', [ApiController::class, 'test_api']);
